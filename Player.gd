8c11f5aa9f4a280ac5c2007696a9340ee77b5bc1
Wilfred James <git@wilfredjames.com>
2023-08-03 16:29:25 +1200

    feat: Initial commit.

---
+++ Player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const VIEW_ANGLE_LIMIT = 1.5
+const MOUSE_SENSITIVITY = 0.001
+
+@onready var head = $Head
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("movement_left", "movement_right", "movement_forward", "movement_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+		head.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
+		head.rotation.x = clamp(head.rotation.x, -VIEW_ANGLE_LIMIT, VIEW_ANGLE_LIMIT)
---
+++ README.md
@@ -0,0 +1,11 @@
+This project contains three cubes for the player to collide against.
+
+1. Imported cube with a `ConcavePolygonShape3D` collider
+1. Imported cube with a `ConvexPolygonShape3D` collider
+1. In-engine cube with a `BoxShape3D` collider
+
+The cylinder `CharacterBody3D` only gets stuck on the corners of the first.
+
+## Controls
+WSAD and mouse look.
+
